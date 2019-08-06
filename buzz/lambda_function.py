5c07049735408fca1c5d6a01627ab07f69da6c5b
Yergeni Cabranes <ycabranes@ferrismarketing.com>
2019-08-06 12:02:24 -0400

    Add 'yml' files

--- buzz/lambda_function.py
+++ buzz/lambda_function.py
@@ -14,11 +14,11 @@ def sample(l, n = 1):
         return result[0]
     return result
 
-def generate_buzz():
+def lambda_handler():
     buzz_terms = sample(buzz, 2)
     phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
         sample(verbs), buzz_terms[1]])
     return phrase.title()
 
 if __name__ == "__main__":
-    print(generate_buzz())
+    lambda_handler()
